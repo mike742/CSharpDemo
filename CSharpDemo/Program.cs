d1893aff3224392f889c0a3a594d98ef740e9484
MikeZorin <mykhailo.zorin@gmail.com>
2021-03-15 10:46:32 -0500

    Add project files.

---
+++ CSharpDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ctrl + F5
+            Console.WriteLine("Hello .Net Core World!");
+        }
+    }
+}
