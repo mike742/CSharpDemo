f5e217e7bd25615e54632f4bdd3ef1e8c461b781
MikeZorin <mykhailo.zorin@gmail.com>
2021-03-17 12:42:48 -0500

    added new c# .Net Core project

---
+++ NewProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
