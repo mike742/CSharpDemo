119e0d02cc3d9ce3379b9903420a52d4476fa5bb
MikeZorin <mykhailo.zorin@gmail.com>
2021-03-19 09:59:24 -0500

    added Nullable types

--- NewProject/Program.cs
+++ NewProject/Program.cs
@@ -6,7 +6,34 @@ namespace NewProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // bool b = null; // true / false / null
+            // int a = null; 
+
+            int availableTickets;
+            int? ticketsOnSale = null;
+            /*
+            if (ticketsOnSale == null)
+            {
+                availableTickets = 0;
+            }
+            else
+            {
+                availableTickets = (int)ticketsOnSale;
+            }
+            */
+            // ? :
+
+            //availableTickets = (ticketsOnSale == null) ? 0 : (int)ticketsOnSale;
+
+            // null coalesce operator
+            availableTickets = ticketsOnSale ?? 0;
+
+
+            // Console.WriteLine("availableTickets = " + availableTickets);
+            // Console.WriteLine("availableTickets = {0}", availableTickets);
+            Console.WriteLine($"availableTickets = {availableTickets}");
+
+            Console.WriteLine("Have a great day");
         }
     }
 }
