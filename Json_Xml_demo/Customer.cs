965ebf4e483300d747605fd528c301e6d2b9ac0f
MikeZorin <mykhailo.zorin@gmail.com>
2021-04-12 12:43:30 -0500

    added Customer example

---
+++ Json_Xml_demo/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Json_Xml_demo
+{
+    public class Customer
+    {
+        public string Name { set; get; }
+        public string CreditCard { set; get; }
+        public string Password { set; get; }
+
+    }
+}
--- Json_Xml_demo/Program.cs
+++ Json_Xml_demo/Program.cs
@@ -169,6 +169,37 @@ namespace Json_Xml_demo
 
             Console.WriteLine($"decriptedCreditCard = {decriptedCreditCard}");
 
+            string password = "Pa$$w0rd";
+            Console.WriteLine($"SaltAndHashed password = {Cripto.Cripto.SaltAndHash(password)}");
+
+
+
+            // Question #2
+
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer { Name = "Bob Smith", CreditCard = "1234-5678-9012-3456", Password = "Pa$$w0rd" },
+                new Customer { Name = "Lucy Johnson", CreditCard = "5252-5678-7845-3456", Password = "123456" },
+            };
+
+            foreach (var item in customers)
+            {
+                item.CreditCard = 
+                    Cripto.Cripto.EncryptString(key, item.CreditCard);
+                item.Password =
+                    Cripto.Cripto.SaltAndHash(item.Password);
+            }
+
+            ToXml(customers, "customers.xml");
+
+            var customersFromXml = FromXml<List<Customer>>("customers.xml");
+
+            Console.WriteLine("===========================================");
+            foreach (var item in customersFromXml)
+            {
+                var ccn = Cripto.Cripto.DecryptString(key, item.CreditCard);
+                Console.WriteLine($"{item.Name} { ccn } {item.Password}");
+            }
         }
 
         public static void ToXml<T>(T obj, string path)
